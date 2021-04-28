a11f004a2d1a575b64a86a94a41e1a9aebc52306
Brian Jackson <brianjackson1224@gmail.com>
2021-04-27 21:02:24 -0700

    Added business test folder

--- SecretSanta/test/SecretSanta.Api.tests/Controllers/UsersControllerTests.cs
+++ SecretSanta/test/SecretSanta.Api.tests/Controllers/UsersControllerTests.cs
@@ -17,13 +17,62 @@ namespace SecretSanta.Api.Tests.Controllers
         public void Get_WithData_ReturnsUsers()
         {
             // Arrange
-            UsersController controller = new();
+            UsersController controller = new(new UserRepository());
 
             // Act
-            IEnumerable<string> events = controller.Get();
+            IEnumerable<User> users = controller.Get();
 
             // Assert
             Assert.IsTrue(users.Any());
+            
+        }
+
+        [TestMethod]
+        [DataRow(42)]
+        public void Get_WithId_ReturnsUserRepositoryUser(int id) {
+            // Arrange
+            TestableUserRepository repo = new();
+            UsersController controller = new(repo);
+            User expectedUser = new();
+            repo.GetItemUser = expectedUser;
+
+            // Act
+            ActionResult<User?> result = controller.Get(id);
+
+            // Assert
+            Assert.AreEqual(id, repo.GetItemId);
+            Assert.AreEqual(expectedUser, result.Value);
+        }
+
+         private class TestableUserRepository : IUserRepository
+        {
+            public User Create(User item)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public User? GetItemUser{ get; set; }
+            public int GetItemId { get; set; }
+            public User? GetItem(int id)
+            {
+                GetItemId = id;
+                return GetItemUser;
+            }
+
+            public ICollection<User> List()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public bool Remove(int id)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public void Save(User item)
+            {
+                throw new System.NotImplementedException();
+            }
         }
     }
 }
