44e8af61cac7f36118afdf0595a020743ad5a4f4
Brian Jackson <brianjackson1224@gmail.com>
2021-06-07 18:05:10 -0700

    Deleted stuff that I never needed :(

--- SecretSanta/src/SecretSanta.Web/Controllers/UsersController.cs
+++ SecretSanta/src/SecretSanta.Web/Controllers/UsersController.cs
@@ -22,52 +22,6 @@ namespace SecretSanta.Web.Controllers
             return View();
         }
 
-        public IActionResult Gifts()
-        {
-            var gifts = MockData.Gifts.OrderBy(g => g.Priority).ToList();
-            return View(gifts);
-        }
-
-        [HttpPost]
-        public IActionResult GiftsDelete(int id)
-        {
-            MockData.Gifts.Remove(MockData.Gifts.Single(g => g.Id == id));
-            return RedirectToAction(nameof(Gifts));
-        }
-
-        public IActionResult CreateGift()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult CreateGift(GiftViewModel viewModel)
-        {
-            if (ModelState.IsValid)
-            {
-                viewModel.Id = MockData.Gifts.Max(g => g.Id) + 1;
-                MockData.Gifts.Add(viewModel);
-                return RedirectToAction(nameof(Gifts));
-            }
-
-            return View(viewModel);
-        }
-
-        public IActionResult EditGift(int id)
-        {
-            return View(MockData.Gifts.Single(g => g.Id == id));
-        }
-
-        [HttpPost]
-        public IActionResult EditGift(GiftViewModel viewModel)
-        {
-            if (ModelState.IsValid)
-            {
-                MockData.Gifts[MockData.Gifts.FindIndex(g => g.Id == viewModel.Id)] = viewModel;
-                return RedirectToAction(nameof(Gifts));
-            }
-
-            return View(viewModel);
-        }
+        
     }
 }
